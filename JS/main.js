8d0d3642af031967f3b2677fc902db94e6625622
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-08 17:23:03 +0100

    add: color prompt

--- JS/main.js
+++ JS/main.js
@@ -2,5 +2,8 @@ const firstName = prompt (' Qual è il tuo nome?');
 console.log(firstName);
 
 const surname = prompt (' Qual è il tuo cognome?');
-console.log(surname)
+console.log(surname); 
+
+const color = prompt (' Qual è il tuo colore preferito?');
+console.log(color);
 
